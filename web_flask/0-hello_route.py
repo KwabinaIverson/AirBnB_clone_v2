f06c15788996942edfdadc673c5558c35a2a1495
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-12-27 03:54:34 -0800

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
+"""
+This script starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000, and has a single route:
+- /: displays "Hello HBNB!"
+
+The option strict_slashes=False is used in the route definition.
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-
-def hbnb():
-    """Displays a message to the web page"""
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when the root route is accessed.
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
