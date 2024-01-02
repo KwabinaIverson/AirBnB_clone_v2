a937dda55b2ebb9911eb8ef38b023279ff6d41a8
Benjamin Atsu <Bredrenbonish@gmail.com>
2024-01-01 17:22:05 -0800

    Static web

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,24 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
 
+# Install Nginx if it's not already installed
 apt-get update
-apt-get install -y nginx
+apt-get -y install nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create the necessary directory structure
+mkdir -p /data/web_static/{releases/test,shared}
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake HTML file for testing
+echo '<html> <head> </head> <body> Holberton School </body> </html>' > /data/web_static/releases/test/index.html
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://github.com/besthor;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+# Correctly manage symbolic links
+ln -snf /data/web_static/releases/test /data/web_static/current
 
+# Set ownership and permissions
+chown -hR ubuntu:ubuntu /data
+
+# Update Nginx configuration
+sed -i '51 i \
+\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
+# Restart Nginx
 service nginx restart
